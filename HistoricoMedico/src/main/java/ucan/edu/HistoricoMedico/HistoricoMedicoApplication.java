b524a34d3ba48a6f9cfa883aa96e89a68277da55
Creuma Kuzola <creumakuzola@gmail.com>
2023-11-30 15:18:59 +0100

    feat: creating repository project

---
+++ HistoricoMedico/src/main/java/ucan/edu/HistoricoMedico/HistoricoMedicoApplication.java
@@ -0,0 +1,13 @@
+package ucan.edu.HistoricoMedico;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HistoricoMedicoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HistoricoMedicoApplication.class, args);
+	}
+
+}
---
+++ HistoricoMedico/src/test/java/ucan/edu/HistoricoMedico/HistoricoMedicoApplicationTests.java
@@ -0,0 +1,13 @@
+package ucan.edu.HistoricoMedico;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HistoricoMedicoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
